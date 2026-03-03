3505354a8a3124afa64d1489ff94f7c77b9314fc
Pedro Miller Rabinovitch <pedro@robotogames.com>
2026-03-03 17:58:20 -0500

    Fix

--- pull_latest.sh
+++ pull_latest.sh
@@ -1,4 +1,4 @@
 COMMIT_HASH=$(git rev-parse HEAD)
 git pull
 # Replace a placeholder string in your HTML file with the hash
-sed -i "s/GIT_VERSION/$COMMIT_HASH/" index.html
+sed -i "s/GIT_VERSION/$COMMIT_HASH/" game.html
