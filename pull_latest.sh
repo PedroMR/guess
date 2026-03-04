713e3fa3fa102601fe50d2cad27167a1b16eb16c
Pedro Miller Rabinovitch <pedro@robotogames.com>
2026-03-03 19:01:27 -0500

    Version info works, I think

--- pull_latest.sh
+++ pull_latest.sh
@@ -1,4 +1,11 @@
-COMMIT_HASH=$(git rev-parse HEAD)
-git pull
-# Replace a placeholder string in your HTML file with the hash
-sed -i "s/GIT_VERSION/$COMMIT_HASH/" game.html
+
+git pull && echo "Pulled."
+
+#COMMIT_HASH=$(git rev-parse HEAD)
+COMMIT_HASH=$(git log --pretty="%h %ad" -1 game.html)
+
+echo $COMMIT_HASH
+if [ -n "$COMMIT_HASH" ]; then
+    # Replace a placeholder string in your HTML file with the hash
+    sed -i "s/%Version.*%/%Version $COMMIT_HASH%/" game.html
+fi
